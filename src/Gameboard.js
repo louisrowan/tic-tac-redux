e0dd4a94402f5dbcf0a29ebe975bd94b41e578af
Louis Rowan <louis.rowan@icloud.com>
2017-03-21 20:23:39 -0700

    adds replay if tie button

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -4,6 +4,11 @@ const { connect } = require('react-redux')
 const { handleTileClick, handleSwitchTurn, handleCheckWinner, handlePlayAgain } = require('./redux/actionCreators')
 
 const Gameboard = React.createClass({
+  getInitialState(){
+    return {
+      moves: 0
+    }
+  },
   handleClick(e){
     if (this.props.winner) return
     var index = e.target.id
@@ -16,23 +21,26 @@ const Gameboard = React.createClass({
     }
   },
   handleAcceptClick(index){
+    this.setState({ moves: this.state.moves + 1})
     this.props.dispatchHandleTileClick(index)
     this.props.dispatchHandleCheckWinner()
     this.props.dispatchHandleSwitchTurn()
   },
   playAgain(){
     this.props.dispatchHandlePlayAgain()
+    this.setState({ moves : 0})
   },
   render(){
     var { tiles, turn, winner } = this.props
+    var { moves } = this.state
     return (
       <div>
         {
-          !winner && turn &&
+          !winner && moves < 9 && turn &&
           <h1 style={{color: 'blue'}}>Player 1's turn</h1>
         }
         {
-          !winner && !turn &&
+          !winner && moves < 9 && !turn &&
           <h1 style={{color: 'red'}}>Player 2's turn</h1>
         }
         {
@@ -42,6 +50,10 @@ const Gameboard = React.createClass({
         {
           winner && !turn &&
           <h1>Player 1 wins!</h1>
+        }
+        {
+          moves >= 9 &&
+          <h1>It's a tie!</h1>
         }
       <div id='gameboard-div' onClick={this.handleClick}>
         {
@@ -57,7 +69,7 @@ const Gameboard = React.createClass({
         }
         </div>
         {
-          winner &&
+          (moves >= 9|| winner) &&
           <button onClick={this.playAgain}>Play again?</button>
         }
     </div>
