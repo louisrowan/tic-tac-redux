ffd10af35ccd961afbb60584a038afccfcdd4650
Louis Rowan <louis.rowan@icloud.com>
2017-03-21 17:07:09 -0700

    removes unnecesary files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+const React = require('react')
+const ReactDOM = require('react-dom')
+const App = require('./App')
+require('./index.css')
 
 ReactDOM.render(
   <App />,
